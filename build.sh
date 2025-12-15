93ca37276b3e3f4684017796dcb68aa60e028632
Matthew Russo <matthew@matthewclayrusso.com>
2025-12-14 19:03:51 -0800

    Init : create package structure

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eaux
+
+export STARTING_DIR="$(pwd)"
+export INSTALL_PREFIX="$HOME/.local"
+export BUILD_DIR="cmake-build-release"
+export CC="clang"
+export CXX="clang++"
+
+function cleanup {
+  cd $STARTING_DIR
+}
+trap cleanup EXIT
+
+cmake -S . -B $BUILD_DIR
+cmake --build $BUILD_DIR --config Release
+cd $BUILD_DIR
+ctest -V
+
+cd ..
+
+cmake --install $BUILD_DIR --prefix "$INSTALL_PREFIX" --config Release
